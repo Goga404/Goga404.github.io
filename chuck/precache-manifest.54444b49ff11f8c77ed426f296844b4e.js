aeab5f64fb3e2a0eca1124b4911b02eb561a7119
Stanislav Koshenkov <justanothergoga@gmail.com>
2019-12-13 15:01:28 +0300

    Add files via upload

---
+++ chuck/precache-manifest.54444b49ff11f8c77ed426f296844b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e1bf988e4d409469d66bfca96342af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf1ab3bdc68595a982a3",
+    "url": "/static/css/main.738997c6.chunk.css"
+  },
+  {
+    "revision": "e6597ce20cd039227032",
+    "url": "/static/js/2.03e8052c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.03e8052c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf1ab3bdc68595a982a3",
+    "url": "/static/js/main.4cec205c.chunk.js"
+  },
+  {
+    "revision": "c893e451f94ef40cfa8e",
+    "url": "/static/js/runtime-main.5aebcf57.js"
+  }
+]);
